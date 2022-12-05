500ad5005552aa743883b133fbf71eca7e14dd78
alnumanqa <alnumanqa@gmail.com>
2022-12-05 09:57:38 -0500

    Modify

--- src/oopExam/Iphone6.java
+++ src/oopExam/Iphone6.java
@@ -51,24 +51,4 @@ public class Iphone6 extends Iphone5 {
 		return total5;
 	}
 
-	/*
-	 * First write about method over loading and method overriding with multiple
-	 * line comments here Now, Think about Iphone6. It also has 6 methods with the
-	 * same name as materials(). For example -- They are return type parameterized
-	 * method (int ramPrice, int cameraPrice), another one return type parameterized
-	 * method (int ramPrice, int cameraPrice, String ProcessorPrice), etc. Can you
-	 * use the void, parameterized(return type), final (return type), or static
-	 * (return type) method here as the same method name? if yes, create them and
-	 * take more int variable of your own. What kind of concept you can use?
-	 * [points: 70]. Instantiate the class in TestPhone and initialize those
-	 * methods. [points: 70]. Now think about a regular class Name ConfiguredIphone6
-	 * which extends Iphone6. Override those methods of Iphone6 and change the logic
-	 * at your own to show the changes. [points: 50]. Show the output in TestPhone
-	 * (how many methods is possible to extends). [points: 50].
-	 * 
-	 * Please make sure you organize the code in every class
-	 * 
-	 * Paste the github link for the package below
-	 * 
-	 */
 }
